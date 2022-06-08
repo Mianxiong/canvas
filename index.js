bc18b0ed542ffcb32a4ce9158b8ae4b8bce950a2
Mianxiong <757134184@qq.com>
2022-06-09 01:47:55 +0800

    画板（第一版）

--- index.js
+++ index.js
@@ -23,38 +23,68 @@
 //  改为使用canvas
 
 //画线
-var canvas = document.getElementById("canvas");
+let canvas = document.getElementById("canvas");
 // 获取浏览器窗口文档显示区域的宽度，不包括滚动条。
 canvas.width = document.documentElement.clientWidth
 // 获取浏览器窗口文档显示区域的高度，不包括滚动条。
 canvas.height = document.documentElement.clientHeight
 
-var ctx = canvas.getContext("2d");
-
-let painting = false
-
+let ctx = canvas.getContext("2d");
 ctx.fillStyle = "black"
 ctx.strokeStyle = "none"
+ctx.lineWidth = 8
+ctx.lineCap = "round"
 
-canvas.onmousedown = () => {
-    painting = true
-}
+let painting = false
+let last
 
+let isTouchDevice = 'ontouchstart' in document.documentElement;
+// console.log(isTouchDevice)
 
-canvas.onmousemove = (e) => {
-    // console.log(e.clientX)
-    // console.log(e.clientY)
-    if (painting === true) {
-        // ctx.fillRect(e.clientX - 5, e.clientY - 5, 10, 10);
-        ctx.beginPath();
-        ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill()
-    } else {
-        console.log('什么都不做')
+if (isTouchDevice) {
+    canvas.ontouchstart = (e) => {
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        last = [x, y]
+    }
+    canvas.ontouchmove = (e) => {
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        // ctx.beginPath();
+        // ctx.arc(x, y, 10, 0, 2 * Math.PI);
+        // ctx.stroke();
+        // ctx.fill()
+        drawLine(last[0], last[1], x, y)
+        last = [x, y]
+    }
+} else {
+    canvas.onmousedown = (e) => {
+        painting = true
+        last = [e.clientX, e.clientY]
+    }
+
+    canvas.onmousemove = (e) => {
+        // console.log(e.clientX)
+        // console.log(e.clientY)
+        if (painting === true) {
+            // ctx.fillRect(e.clientX - 5, e.clientY - 5, 10, 10);
+            // ctx.beginPath();
+            // ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI);
+            // ctx.stroke();
+            // ctx.fill()
+            drawLine(last[0], last[1], e.clientX, e.clientY)
+            last = [e.clientX, e.clientY]
+        }
+    }
+
+    canvas.onmouseup = () => {
+        painting = false
     }
 }
 
-canvas.onmouseup = () => {
-    painting = false
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.stroke();
 }
